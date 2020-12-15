c186224dcd13b1d82356c8c8e6db13e90b54f244
Anna Shmeleva <aniuta.shmeleva@yandex.ru>
2020-12-15 10:42:44 +0300

    hotfixes

--- script.js
+++ script.js
@@ -3,33 +3,31 @@ let editButton = document.querySelector('.profile__edit-button');
 let closePopup = document.querySelector('.popup__close');
 let popupName = document.querySelector('#popup-name');
 let popupJob = document.querySelector('#popup-job');
+let nameUser = document.querySelector('.profile__name');
+let jobUser = document.querySelector('.profile__info');
+let submitForm = document.querySelector('.popup__container');
 
-editButton.addEventListener('click', function() {
-  let nameUser = document.querySelector('.profile__name').textContent;
-  let jobUser = document.querySelector('.profile__info').textContent;
-  popupName.value = nameUser;
-  popupJob.value = jobUser;
-  callPopup.classList.remove('popup');
+function editButtonClick() {
+  popupName.value = nameUser.textContent;
+  popupJob.value = jobUser.textContent;
   callPopup.classList.add('popup_opened');
-});
+}
 
-closePopup.addEventListener('click', function() {
+editButton.addEventListener('click', editButtonClick);
+
+function closePopupClick() {
   callPopup.classList.remove('popup_opened');
-  callPopup.classList.add('popup');
-});
+}
 
-let submitButton = document.querySelector('.popup__submit');
+closePopup.addEventListener('click', closePopupClick);
 
 function handleFormSubmit (evt) {
   evt.preventDefault();
 
-  let nameUser = document.querySelector('.profile__name');
-  let jobUser = document.querySelector('.profile__info');
   nameUser.textContent = popupName.value;
   jobUser.textContent = popupJob.value;
-  callPopup.classList.remove('popup_opened');
-  callPopup.classList.add('popup');
-};
+  closePopupClick();
+}
 
-submitButton.addEventListener('click', handleFormSubmit); //'submit' работает некорректно, evt.preventDefault() - не отменяет его действие
+submitForm.addEventListener('submit', handleFormSubmit);
 
